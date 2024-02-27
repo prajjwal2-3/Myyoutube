2dad45838aaab6b31f8f8ff3489c73948904e50d
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-02-27 19:14:48 +0530

    autoplay

--- src/components/Videocard.js
+++ src/components/Videocard.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 const Videocard = ({array}) => {
     console.log(array)
+    const key="RK1J8kC1sEY"
   return (
    <div className="">
       
          <div className='aspect-video m-2 '>
-         <iframe className='rounded-xl' src={`https://www.youtube.com/embed/${array.id?.videoId}?si=roF6w1-ZfInPoufF&amp;controls=0`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+         <iframe className='rounded-xl' src={`https://www.youtube.com/embed/${key}?si=roF6w1-ZfInPoufF&amp&controls=0&mute=1&autoplay=1`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
      
    </div>
