1aec82908ba02db3d8d11be011177154e2a80740
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-02-29 22:57:21 +0530

    watchpage started

--- src/components/Watchpage.js
+++ src/components/Watchpage.js
@@ -2,14 +2,19 @@ import React from 'react'
 
 const Watchpage = () => {
   return (
-    <div className='flex'>
-      <div className="video-comment w-8/12 h-screen  bg-blue-400">
-        h
-        <div className="video"></div>
+   <div className="flex justify-center">
+     <div className='flex  w-11/12 '>
+      <div className="video-comment w-8/12 h-screen ">
+        
+        <div className="video aspect-video mt-5 mr-1 ">
+
+        <iframe className='w-full h-5/6 rounded-xl' src="https://www.youtube.com/embed/dfTj3aslNLU?si=Xl13FNhU_b4gBdWZ&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
         <div className="comment"></div>
       </div>
-      <div className="recommended w-4/12 bg-red-600">j</div>
+      <div className="recommended w-4/12 ">j</div>
     </div>
+   </div>
   )
 }
 
