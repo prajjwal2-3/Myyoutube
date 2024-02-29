611c491771321153baf8d000102a9c73a40ffa25
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-02-29 18:25:09 +0530

    che

--- src/Utils/useChanneldata.js
+++ src/Utils/useChanneldata.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import { APIKEY } from './Constants'
-const useChanneldata = () => {
+// import React from 'react'
+// import { APIKEY } from './Constants'
+// const useChanneldata = () => {
 
-    const youtube = async ()=>{
-            try{
-              const data= await fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics%2Cstatus&id=UCkWOpf05w9dPthQ-2tXndfA&key=${APIKEY}`)
-              const json = await data.json();
-              const rough= await Promise.all(json)
-              console.log(channel)
-              console.log(json)
-            //   dispatch(addrecomvideo(json.items))
+//     const youtube = async ()=>{
+//             try{
+//               const data= await fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics%2Cstatus&id=UCkWOpf05w9dPthQ-2tXndfA&key=${APIKEY}`)
+//               const json = await data.json();
+//               const rough= await Promise.all(json)
+//               console.log(channel)
+//               console.log(json)
+//             //   dispatch(addrecomvideo(json.items))
              
-            }catch(error){
-              console.log(error)
-            }
+//             }catch(error){
+//               console.log(error)
+//             }
            
-          }
-  return (
-    {youtube}
-  )
-}
+//           }
+//   return (
+//     {youtube}
+//   )
+// }
 
-export default useChanneldata
+// export default useChanneldata
